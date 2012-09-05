7bc9d778ddb1ded12bd011a049efacc99fe39b83
Charles Desneuf <c.desneuf@gmail.com>
2012-09-05 22:31:38 +0200

    Remove useless w100

--- application/views/account/view.blade.php
+++ application/views/account/view.blade.php
@@ -1,6 +1,6 @@
 <h1>Mon compte</h1>
 <div class="line">
-	<div class="mod w100">
+	<div class="mod">
 	{{ HTML::image('avatars/'. Auth::user()->image, Auth::user()->firstname . ' ' . Auth::user()->lastname .' photo', array('class' => 'left')) }}
 		<p>{{ Auth::user()->firstname }} {{ Auth::user()->lastname }}</p>
 		<p>{{ Auth::user()->email }}</p>
@@ -9,7 +9,7 @@
 </div>
 
 <div class"line">
-	<div class="mod w100">
+	<div class="mod">
 		<h2>Changer de mot de passe</h2>
 		<p>Il vous est possible de changer votre mot de passe.</p>
 		<p>{{ HTML::link('account/change_pwd', 'Changer de mot de passe', array('class' => 'button right')) }}</p>
