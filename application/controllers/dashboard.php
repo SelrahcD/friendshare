23b6965cad0e3df4e8d17d6fbd5ff00c45785809
Charles Desneuf <c.desneuf@gmail.com>
2012-09-06 01:01:06 +0200

    Display some friends, credit, debit, and friends number

--- application/controllers/dashboard.php
+++ application/controllers/dashboard.php
@@ -10,6 +10,18 @@ class Dashboard_Controller extends Controller {
 	}
 	
 	public function action_index(){
+		$user = Auth::user();
+
 		$this->layout->content = View::make('dashboard');
+
+		/* Add number of friends to the view */
+		$friendsNumber = $user->friends()->count();
+		$this->layout->content->with('friendsNumber', $friendsNumber);
+
+		
+		/* Get the five last friends user had interaction with */
+		$friends = $user->friends()->order_by('updated_at', 'desc')->take(5)->get();
+		$this->layout->content->with('friends', $friends);
+
 	}
 }
--- application/views/dashboard.blade.php
+++ application/views/dashboard.blade.php
@@ -1,20 +1,38 @@
 <h1>Dashboard</h1>
 <div class="row txtcenter stat-container">
-	<div class="col w200p"><p>Vous avez <span class="stat-number neutral">{{ HTML::link('friends', '7') }}</span> amis</p>
+	<div class="col w200p"><p>Vous avez <span class="stat-number neutral">{{ HTML::link('friends', $friendsNumber) }}</span>
+	@if($friendsNumber > 1)
+	 amis
+	 @else 
+	 ami
+	 @endif</p>
 	</div>
-	<div class="col w200p"><p>Vous devez <span class="stat-number negative">16,46€</span></p>
+	<div class="col w200p"><p>Vous devez <span class="stat-number negative">{{ abs(Auth::user()->debit) }}</span></p>
 	</div>
-	<div class="col w200p"><p>On vous doit <span class="stat-number positive">12€24</span></p>
+	<div class="col w200p"><p>On vous doit <span class="stat-number positive">{{ abs(Auth::user()->credit) }}</span></p>
 	</div>
 </div>
 <div class="line">
 	<div class="mod clearfix w400p">
 	<h2>Amis</h2>
-	<ul class="friends-list">
-		<li>{{ HTML::image('avatars/'. Auth::user()->image('small'), Auth::user()->firstname . ' ' . Auth::user()->lastname .' photo', array('class' => 'avatar')) }}<p class="inbl">Vous devez <span class="negative">12€25</span> à Martin Alain</p></li>
-		<li><img src="http://placehold.it/50x50" class="avatar" /><p class="inbl">Vous devez <span class="negative">3€30</span> à Bruce Willis</p></li>
-		<li><img src="http://placehold.it/50x50" class="avatar" /><p class="inbl">Jean-Maxime Devaux vous doit <span class="positive">37€20</span></p></li>
-	</ul>
+	@if(sizeof($friends) > 0)
+		<table class="friends-table">
+			@foreach($friends as $friend)
+			<tr>
+				<td class="small-photo"><a href="#">{{ HTML::image('avatars/'. $friend->image('small'), $friend->firstname . ' ' . $friend->lastname .' photo') }}</a></td>
+				<td>
+				@if($friend->pivot->debt >= 0)
+					{{ $friend->firstname }} {{ $friend->lastname }} vous doit <span class="positive">{{ abs($friend->pivot->debt) }}</span>.
+				@else
+				Vous devez <span class="negative">{{ abs($friend->pivot->debt) }}</span> à {{ $friend->firstname }} {{ $friend->lastname }}.
+				@endif
+				</td>
+			</tr>
+			@endforeach
+		</table>
+		@else
+		<p>Vous n'avez pas d'amis. N'hésitez pas à {{ HTML::link('friends', 'en ajouter !')}}</p>
+		@endif
 	<p>
 	{{ HTML::link('friends', 'Tous les amis', array('class' => 'button right'))}}
 	</p>
